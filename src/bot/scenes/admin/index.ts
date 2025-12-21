5cd385eabf4dad2384b126621cf205ae69537a47
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-12-21 13:28:59 +0500

    scene lar alohida fayillarga ajratildi

--- src/bot/scenes/admin/index.ts
+++ src/bot/scenes/admin/index.ts
@@ -1,19 +1,11 @@
 import { Scenes } from "telegraf";
 import { IMyContext } from "@bot/my-context";
-import { addAdminScene, changeRoleScenes, settingsScenes, superAdminScene } from "./super.admin.scene";
 import { addProductWizard, adminScene, orderScene, productScene, statisticsScene } from "./admin.scene";
-import { addCategoryWizard, categoryScene } from "./category.scene";
 
 export const adminStage: Scenes.BaseScene<IMyContext>[] = [
-	superAdminScene,
-	addAdminScene,
-	settingsScenes,
-	changeRoleScenes,
 	adminScene,
 	productScene,
 	orderScene,
 	statisticsScene,
-	addProductWizard,
-	categoryScene,
-	addCategoryWizard
+	addProductWizard
 ];
