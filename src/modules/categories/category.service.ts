7dedffd7cc2c89048e15ff9da6f53c0ba1e24710
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-12-31 16:48:41 +0500

    categ delete func add

--- src/modules/categories/category.service.ts
+++ src/modules/categories/category.service.ts
@@ -1,6 +1,7 @@
 import { IMyContext } from "@bot/my-context"
 import { Category } from "db/entities/category.entity"
 import { categoryRepo } from "db/repositories";
+import { REPL_MODE_SLOPPY } from "repl";
 
 export const createCategory = async () => {
 
@@ -24,4 +25,24 @@ export const categoryList = async (ctx: IMyContext): Promise<Category[]> => {
 		console.error(error.message);
 		await ctx.reply('Nimadir xato ketdi qaytsa urinib koring!🙏');
 	};
+};
+
+export const deleteCategoryByID = async (
+	ctx: IMyContext,
+	id: string
+): Promise<boolean> => {
+	try {
+		const category = await categoryRepo.findOne({ where: { id } });
+
+		if (!category) return false;
+
+		const result = await categoryRepo.delete(id);
+
+		return (result.affected ?? 0) > 0;
+
+	} catch (error: any) {
+		console.error(error.message);
+		await ctx.reply("Nimadir xato ketdi, qayta urinib ko‘ring 🙏");
+		return false;
+	}
 };
