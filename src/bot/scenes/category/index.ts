f16822b5a04d17601b23657333f40bb7b1ea5feb
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-12-31 16:33:52 +0500

    add va delete wizard lar qoshildi kategory stage ga

--- src/bot/scenes/category/index.ts
+++ src/bot/scenes/category/index.ts
@@ -1,9 +1,10 @@
 import { IMyContext } from "@bot/my-context";
 import { Scenes } from "telegraf";
-import { addCategoryWizard, categoryScene } from "./category.scene";
+import { addCategoryWizard, categoryScene, deleteCategoryWizard } from "./category.scene";
 
 
 export const categoryStage: Scenes.BaseScene<IMyContext>[] = [
 	categoryScene,
-	addCategoryWizard
+	addCategoryWizard,
+	deleteCategoryWizard
 ];
