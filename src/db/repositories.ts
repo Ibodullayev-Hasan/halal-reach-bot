b4b3cea93106880739ae3212729d4cac74e6b45d
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-12-31 16:45:43 +0500

    order repo chaqirildi

--- src/db/repositories.ts
+++ src/db/repositories.ts
@@ -3,7 +3,9 @@ import { User } from "./entities/user.entity";
 import { AppDataSource } from "./data-source";
 import { Category } from "./entities/category.entity";
 import { Product } from "./entities/product.entity";
+import { Order } from "./entities/order.entity";
 
 export const userRepo: Repository<User> = AppDataSource.getRepository(User);
 export const categoryRepo: Repository<Category> = AppDataSource.getRepository(Category);
 export const productRepo: Repository<Product> = AppDataSource.getRepository(Product);
+export const orderRepo: Repository<Order> = AppDataSource.getRepository(Order);
